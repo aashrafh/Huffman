cbd976c8fa7f4f3f9d5346ff2f3c61c60a3807cb
aashrafh <ahmed.ahmed991@eng-st.cu.edu.eg>
2020-03-16 01:20:50 +0200

    Decoder

--- Main.py
+++ Main.py
@@ -1,35 +1,46 @@
-from ReadInput import Input as I
-from huffman_tree import HuffmanTree 
+from huffman_tree import HuffmanTree
 
-def recurse (Node,code = ''):
-    if Node.symbol != None:
-        encodingArray[Node.symbol] = code
-        return
-    recurse(Node.right,code + '1')
-    recurse(Node.left,code + '0')
-    
-    
-    
-Input = I('input.txt')
-Input2 = I('input.txt')
-probability = Input.constructProbability()
-encodingArray = probability.copy()
+
+def construct_probability(input_string):
+    freq = {}
+    for char in input_string:
+        if char not in freq:
+            freq[char] = 1
+        else:
+            freq[char] = freq[char]+1
+
+    return freq
+
+
+input_string = open("input.txt", "r").read()
+probability = construct_probability(input_string)
 tree = HuffmanTree()
 tree.get_nodes_heap(probability)
 tree.construct_tree()
-firstNode = tree.heap[0]
-inputString = Input2.getString()
-if len(probability) == 1:
-    recurse(firstNode,'0')
-else:
-     recurse(firstNode)
-outputString = ''
+encoding_array = tree.get_codes()
 
-print(encodingArray)
-
-for char in inputString:
-    outputString += encodingArray[char]
-
-ouput = open ('output.txt','w+')
-ouput.write(outputString)
+# Encode
+output_string = ""
+for char in input_string:
+    output_string += encoding_array[char]
+ouput = open('binary_output.bin', 'w')
+ouput.write(output_string)
 ouput.close()
+
+# Decode
+decoded_output = ""
+temp_str = ""
+encoded_file = open("binary_output.bin", "rb")
+char = encoded_file.read(1)
+while char:
+    temp_str = temp_str + str(char)
+    if temp_str in encoding_array:
+        print("temp_str")
+        decoded_output = decoded_output + encoding_array[temp_str]
+        temp_str = ""
+    char = encoded_file.read(1)
+
+print(decoded_output)
+decoded_output_file = open('decoded_output.txt', 'w')
+decoded_output_file.write(decoded_output)
+decoded_output_file.close()
--- ReadInput.py
+++ ReadInput.py
@@ -1,20 +1,22 @@
-import collections
-class Input:
-    def __init__(self,fileName):
-        self.file = open(fileName)
-        
-    def getString(self):
-        chars = []
-        
-        for string in self.file:
-            for char in string:
-                chars.append(char)
-        return chars
-    
-    def constructProbability(self):
-        #used to count occurancies of an element in a list
-        freq = collections.Counter(self.getString())
-        chars = {}
-        for char in freq:
-            chars.update({char:freq[char]})
-        return chars
+# import collections
+
+
+# class Input:
+#     def __init__(self, fileName):
+#         self.file =
+
+#     def getString(self):
+#         chars = []
+
+#         for string in self.file:
+#             for char in string:
+#                 chars.append(char)
+#         return chars
+
+#     def constructProbability(self):
+#         # used to count occurancies of an element in a list
+#         freq = collections.Counter(self.getString())
+#         chars = {}
+#         for char in freq:
+#             chars.update({char: freq[char]})
+#         return chars
